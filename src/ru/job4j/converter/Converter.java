d7a64ce9615305237c9e2c9d1d8bcfe39a198d08
mimilun <mimilun@mail.ru>
2020-09-10 10:56:12 +0300

    5. Конвертер валюты [#359041]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -15,7 +15,7 @@ public class Converter {
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        int dollar = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollars.");
     }
 }
