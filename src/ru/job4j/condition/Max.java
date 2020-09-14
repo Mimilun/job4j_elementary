7491a7fd32afdd73b38200626cf431d7ec7fd548
mimilun <mimilun@mail.ru>
2020-09-14 15:35:42 +0300

    4.2. Тернарное сравнение [#359003]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int first, int second) {
+        int result = first > second ? first : second;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(1, 3));
+    }
+}
