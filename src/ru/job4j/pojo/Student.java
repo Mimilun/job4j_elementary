f89347dfdb639f0774f51b09882dfe4cf5b097f6
mimilun <mimilun@mail.ru>
2020-09-30 15:31:12 +0300

    0. Что такое инкапсуляция. [#359231]

---
+++ src/ru/job4j/pojo/Student.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fio;
+    private int group;
+    private String admission;
+
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getAdmission() {
+        return admission;
+    }
+
+    public void setAdmission(String admission) {
+        this.admission = admission;
+    }
+}
